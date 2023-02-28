ea0d7fe82b8b2c1d96faca0437ce7c2e52f068e1
omoleen <Oreoluwa123>
2023-02-28 03:22:27 +0100

    logout view

--- realestateapp/views.py
+++ realestateapp/views.py
@@ -6,6 +6,7 @@ from .models import User, Apartment
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 import random
+from django.contrib.auth import logout
 
 
 def landing(request):
@@ -64,6 +65,7 @@ def loginview(request):
     return render(request, 'login.html')
 
 
+@login_required
 def profile(request):
     if not request.user.is_authenticated:
         return redirect('login')
@@ -73,3 +75,8 @@ def profile(request):
     }
 
     return render(request, 'profile.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('landing')
